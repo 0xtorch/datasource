bcd0a688ab4591f90d6c11ea9f803f069bb10178
0xnekoya <nekoya.bc@gmail.com>
2024-10-02 22:27:23 +0900

    Divide evm test file to multiple files

---
+++ tests/evmAddresses.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, test } from 'bun:test'
+import { readdir } from 'node:fs/promises'
+import { evmAddressWithoutChainIdSchema } from '../scripts/schemas'
+
+describe('Should valid evm chain address json', async () => {
+  const chainDirs = await readdir('./evms/chains')
+  for (const chainDir of chainDirs) {
+    const filenames = await readdir(`./evms/chains/${chainDir}`)
+    for (const filename of filenames) {
+      if (!filename.endsWith('.json')) {
+        continue
+      }
+      test(`evms/chains/${chainDir}/${filename}`, async () => {
+        const json = await Bun.file(
+          `./evms/chains/${chainDir}/${filename}`,
+        ).json()
+        const result = evmAddressWithoutChainIdSchema.safeParse(json)
+        expect(result.success).toBe(true)
+      })
+    }
+  }
+})
+
